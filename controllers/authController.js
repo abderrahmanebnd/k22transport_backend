e0a876725ab07b4e6b8947392adb97c93ba1b2c5
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-02-06 13:08:58 +0100

    refactor: standardize string quotes and improve cookie options in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,9 @@
-const crypto = require("crypto");
-const { promisify } = require("util");
-const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
-const catchAsync = require("../utils/catchAsync.js");
-const AppError = require("../utils/appError.js");
+const crypto = require('crypto');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync.js');
+const AppError = require('../utils/appError.js');
 
 // sign means in details that it will take the payload which is the id and the secret key and the options and return a token
 const signToken = (id) => {
@@ -16,17 +16,17 @@ const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    secure: process.env.NODE_ENV === 'production', // Only set to true in production
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Adjust for cross-site cookies
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
   };
 
-  res.cookie("jwt", token, cookieOptions);
+  res.cookie('jwt', token, cookieOptions);
   user.password = undefined;
   res.status(statusCode).json({
-    status: "success",
+    status: 'success',
     token,
     data: {
       user,
@@ -37,8 +37,8 @@ const createSendToken = (user, statusCode, res) => {
 exports.signup = catchAsync(async (req, res, next) => {
   let { role } = req.body;
 
-  if (role && !["client", "driver"].includes(role)) {
-    return next(new AppError("role must be either driver or client", 400));
+  if (role && !['client', 'driver'].includes(role)) {
+    return next(new AppError('role must be either driver or client', 400));
   }
 
   const newUser = await User.create({
@@ -46,7 +46,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    role: role || "client",
+    role: role || 'client',
   });
 
   createSendToken(newUser, 201, res);
@@ -57,25 +57,25 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 1) Check if email and password exist
   if (!email || !password) {
-    return next(new AppError("Please provide email and password", 400));
+    return next(new AppError('Please provide email and password', 400));
   }
   // 2) Check if the user exists and the password is correct
-  const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user?.correctPassword(password, user.password))) {
-    return next(new AppError("Incorrect email or password", 401)); //unauthorized
+    return next(new AppError('Incorrect email or password', 401)); //unauthorized
   }
   // 3) If everything ok, send token to client
   createSendToken(user, 200, res);
 });
 
 exports.logout = catchAsync(async (req, res, next) => {
-  res.clearCookie("jwt", {
+  res.clearCookie('jwt', {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict',
   });
-  res.status(200).json({ status: "success" });
+  res.status(200).json({ status: 'success' });
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -84,7 +84,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
     return next(
-      new AppError("You are not logged in! Please log in to get access.", 401),
+      new AppError('You are not logged in! Please log in to get access.', 401),
     );
   }
 
@@ -97,7 +97,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   if (!freshUser)
     next(
       new AppError(
-        "The user belonging to this token does no longer exist.",
+        'The user belonging to this token does no longer exist.',
         401,
       ),
     );
@@ -106,7 +106,7 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   if (freshUser.changedPasswordAfter(decoded.iat)) {
     return next(
-      new AppError("User recently changed password! Please log in again", 401),
+      new AppError('User recently changed password! Please log in again', 401),
     );
   } // iat is the time the token was issued
 
@@ -119,7 +119,7 @@ exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return next(
-        new AppError("You do not have permission to perform this action", 403),
+        new AppError('You do not have permission to perform this action', 403),
       );
     }
     next();
