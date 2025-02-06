4333b948d4c5780c279c13f1ddacb572fa20ccf9
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-02-06 12:33:53 +0100

    feat: update CORS configuration for production and add Vercel deployment configuration

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const cors = require("cors");
-const missionRouter = require("./routes/missionRoutes");
-const userRouter = require("./routes/userRoutes");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cors = require('cors');
+const missionRouter = require('./routes/missionRoutes');
+const userRouter = require('./routes/userRoutes');
 
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
-const generalRouter = require("./routes/generalRoutes");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const generalRouter = require('./routes/generalRoutes');
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // origin: "http://localhost:5173",
+    origin: 'https://k22-convoyage.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   }),
 );
@@ -26,11 +27,11 @@ app.use(cookieParser());
 app.use(mongoSanitize());
 app.use(xss());
 
-app.use("/api/v1", generalRouter);
-app.use("/api/v1/missions", missionRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1', generalRouter);
+app.use('/api/v1/missions', missionRouter);
+app.use('/api/v1/users', userRouter);
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(`Can not find ${req.originalUrl} on this server`, 404));
 });
 app.use(globalErrorHandler);
